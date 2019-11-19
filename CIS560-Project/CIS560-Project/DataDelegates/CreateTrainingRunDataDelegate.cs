7127af7df87231d1ba30a626ed2124b729b91a95
mikexc1026 <michaelbrinkman98@ksu.edu>
2019-11-19 15:57:17 -0600

    Create delegate to create training run

---
+++ CIS560-Project/CIS560-Project/DataDelegates/CreateTrainingRunDataDelegate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CIS560_Project.DataDelegates
+{
+    internal class CreateTrainingRunDataDelegate : NonQueryDataDelegate<TrainingRun>
+    {
+        public readonly string date;
+        public readonly string distance;
+        public readonly string time;
+        public readonly string averageHR;
+
+        public CreateTrainingRunDataDelegate(string date, string distance, string time, string averageHR)
+            : base("TrainingRun.CreateTrainingRun")
+        {
+            this.date = date;
+            this.distance = distance;
+            this.time = time;
+            this.averageHR = averageHR;
+
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("Date", date);
+            command.Parameters.AddWithValue("Distance", distance);
+            command.Parameters.AddWithValue("Time", time);
+            command.Parameters.AddWithValue("AverageHR", averageHR);
+
+            var t = command.Parameters.Add("TrainingRunId", SqlDbType.Int);
+            t.Direction = ParameterDirection.Output;
+        }
+
+        public override TrainingRun Translate(SqlCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
