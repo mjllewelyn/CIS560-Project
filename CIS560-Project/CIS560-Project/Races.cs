30f91c32d54a65a2b68871969aac0b184e9a2d9c
mikexc1026 <michaelbrinkman98@ksu.edu>
2019-11-19 12:09:21 -0600

    Make initial races form

---
+++ CIS560-Project/CIS560-Project/Races.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS560_Project
+{
+    public partial class uxRaces : Form
+    {
+        public uxRaces()
+        {
+            InitializeComponent();
+        }
+
+        private void uxEnterARaceButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxBackButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
