fa139bd187dec5b91b5c672d18aa7279e4906e10
mcsmih <31423235+mcsmih@users.noreply.github.com>
2019-12-05 16:59:28 -0600

    Add files via upload
    Added script to create schema and drop tables

---
+++ CIS560-Project/CIS560-Project/Database/Tables/DropTables.sql
@@ -0,0 +1,9 @@
+DROP TABLE CrossCountry.RaceParticipant IF EXISTS;
+DROP TABLE CrossCountry.Race IF EXISTS;
+DROP TABLE CrossCountry.Location IF EXISTS;
+DROP TABLE CrossCountry.TrainingRun IF EXISTS;
+DROP TABLE CrossCountry.Runner IF EXISTS;
+DROP TABLE CrossCountry.Team IF EXISTS;
+DROP TABLE CrossCountry.Coach IF EXISTS;
+DROP TABLE CrossCountry.[User] IF EXISTS;
+GO
