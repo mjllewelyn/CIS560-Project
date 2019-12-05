dd8a1284252998a019c72cfbd3d665b6ba191f29
mcsmih <31423235+mcsmih@users.noreply.github.com>
2019-12-05 17:07:54 -0600

    Add files via upload
    Added a script to rebuild the database

---
+++ CIS560-Project/CIS560-Project/RebuildDatabase.ps1
@@ -0,0 +1,29 @@
+﻿Param(
+   [string] $Server = "(localdb)\MSSQLLocalDb",
+   [string] $Database = "mcsmih"
+)
+
+Import-Module Sqlps -DisableNameChecking;
+
+Write-Host "Rebuilding Database"
+
+Write-Host "Dropping Tables"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\DropTables.sql"
+Write-Host "Creating schema"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\CrossCountry.sql"
+Write-Host "Creating Tables"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\CreateUserTable.sql"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\CreateLocationTable.sql"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\CreateCoachTable.sql"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\CreateTeamTable.sql"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\CreateRunnerTable.sql"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\CreateTrainingRunTable.sql"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\CreateRaceTable.sql"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "Database\Tables\CreateRaceParticipantTable.sql"
+Write-Host "Stored Procedures"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile ""
+
+Write-Host "Inserting Data"
+Invoke-Sqlcmd -ServerInstance $Server -Database $Database -InputFile "DATA\InsertData.sql"
+
+Write-Host "Rebuild Complete."
