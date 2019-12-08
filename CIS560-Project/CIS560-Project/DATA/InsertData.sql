1c074201a7a8cffbf8a91349b4050ac7b71567ec
Matt Llewelyn <mjllewelyn@gmail.com>
2019-12-08 00:38:17 -0600

    Fixed Coach and Runner csv files

--- CIS560-Project/CIS560-Project/DATA/InsertData.sql
+++ CIS560-Project/CIS560-Project/DATA/InsertData.sql
@@ -5,13 +5,13 @@ DECLARE @sql VARCHAR(max)
 SET @sql = 'BULK INSERT CrossCountry.[User] FROM "' + @path + 'User.csv" WITH (FIRSTROW=2, FIELDTERMINATOR='','', ROWTERMINATOR=''\n'')'
 EXEC(@sql)
 
-SET @sql = 'BULK INSERT CrossCountry.[Coach] FROM "' + @path + 'Coach.csv" WITH (FIRSTROW=2, FIELDTERMINATOR='','', ROWTERMINATOR=''\n'')'
+SET @sql = 'BULK INSERT CrossCountry.[Coach] FROM "' + @path + 'Coach.csv" WITH (FIRSTROW=1, FIELDTERMINATOR='','', ROWTERMINATOR=''\n'')'
 EXEC(@sql)
 
 SET @sql = 'BULK INSERT CrossCountry.[Team] FROM "' + @path + 'Team.csv" WITH (FIRSTROW=2, FIELDTERMINATOR='','', ROWTERMINATOR=''\n'')'
 EXEC(@sql)
 
-SET @sql = 'BULK INSERT CrossCountry.[Runner] FROM "' + @path + 'Runner.csv" WITH (FIRSTROW=2, FIELDTERMINATOR='','', ROWTERMINATOR=''\n'')'
+SET @sql = 'BULK INSERT CrossCountry.[Runner] FROM "' + @path + 'Runner.csv" WITH (FIRSTROW=1, FIELDTERMINATOR='','', ROWTERMINATOR=''\n'')'
 EXEC(@sql)
 
 SET @sql = 'BULK INSERT CrossCountry.[TrainingRun] FROM "' + @path + 'TrainingRun.csv" WITH (FIRSTROW=2, FIELDTERMINATOR='','', ROWTERMINATOR=''\n'')'
